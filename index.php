e861a754e33847d2f4fb65d0fd05ed7ced19a77d
Dave Santos <info@davesantos.com>
2011-06-23 15:20:42 -0700

    Added more includes, Deleted underscore folder

--- index.php
+++ index.php
@@ -12,11 +12,8 @@
 				<?php the_content(); ?>
 			</div>
 
-			<footer class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-			</footer>
+			<?php include (TEMPLATEPATH . '/inc/_postmetadata.php' ); ?>
+			
 
 		</article>
 
